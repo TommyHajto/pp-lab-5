f24fc66d1a3f43ed2c6be066c552561705c4890e
AEHTomaszRokoszynski <trokoszynski1pl@gmail.com>
2024-05-05 14:16:09 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+import company.interfaces.Messenger;
+import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("John", 30);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Invalid age provided: " + e.getMessage());
-        }
+        int x = 10;
+        int y = 5;
+        
+        int sum = MathUtils.add(x, y);
+        System.out.println("Sum of " + x + " and " + y + " is: " + sum);
+
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("The sum of " + x + " and " + y + " is: " + sum);
     }
 }
