1447112785ffd3b332ba4265c4ca1b9cf60dec1e
AEHTomaszRokoszynski <trokoszynski1pl@gmail.com>
2024-05-05 14:39:43 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import company.interfaces.Messenger;
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        
+        int sum = MathUtils.add(x, y);
+        System.out.println("Sum of " + x + " and " + y + " is: " + sum);
+
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("test message!");
+        messenger.sendMessage("The sum of " + x + " and " + y + " is: " + sum);
     }
 }
